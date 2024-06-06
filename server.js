c1d9d680c839ce88c7a0884dd836aa5bd15eaa60
kavin-x <kavinkumarab@gmail.com>
2024-06-06 12:41:42 +0530

    feat: removed stringification of data

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ wss.on('connection', (ws) => {
 
     // Handle initial message to set client ID
     ws.on('message', (message) => {
-        console.log(message);
         const decodedMessage = require('msgpack-lite').decode(new Uint8Array(message));
 
         if (!clientId) {
